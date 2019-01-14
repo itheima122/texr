0ff8421159a1db1e0ad1bda22602f7996914c644
itheima <itheima@163.com>
2019-01-14 17:16:29 +0800

    提交到码云

--- test/src/main/dph/test1.java
+++ test/src/main/dph/test1.java
@@ -4,7 +4,6 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("Hello Word1");
         System.out.println("Hello Word2");
-
-
+        System.out.println("Hello Word3");
     }
 }

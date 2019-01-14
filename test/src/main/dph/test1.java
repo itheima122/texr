77dae1dba35e7211668223f656925340219a63c6
itheima <itheima@163.com>
2019-01-14 15:42:25 +0800

    初始化提交代码

--- test/src/main/dph/test1.java
+++ test/src/main/dph/test1.java
@@ -4,6 +4,7 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("Hello Word1");
         System.out.println("Hello Word2");
+        System.out.println("Hello Word3");
 
     }
 }

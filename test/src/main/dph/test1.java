0c0d88718593a9404f548be28bd8e3b7f11b2348
itheima <itheima@163.com>
2019-01-14 15:06:33 +0800

    新增Hello Word2

--- test/src/main/dph/test1.java
+++ test/src/main/dph/test1.java
@@ -3,6 +3,7 @@ package main.dph;
 public class test1 {
     public static void main(String[] args) {
         System.out.println("Hello Word1");
+        System.out.println("Hello Word2");
 
     }
 }
